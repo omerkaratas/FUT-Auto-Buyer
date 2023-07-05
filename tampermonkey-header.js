14d5ea3602e9bd3d79ac6de67f325bba34f2517e
omer <okarat@gmail.com>
2023-07-05 17:50:11 +0300

    Update tampermonkey-header.js

--- tampermonkey-header.js
+++ tampermonkey-header.js
@@ -15,21 +15,17 @@ module.exports = {
       "ea2.com",
       "futbin.com",
       "futwiz.com",
-      "discordapp.com",
-      "futbin.org",
-      "exp.host",
-      "on.aws",
+      "futbin.org"
     ],
     require: [
       "https://code.jquery.com/jquery-3.6.1.min.js",
-      "https://raw.githubusercontent.com/ckalgos/FUT-Auto-Buyer/main/external/discord.11.4.2.min.js",
       "https://greasyfork.org/scripts/47911-font-awesome-all-js/code/Font-awesome%20AllJs.js?version=275337",
       "https://github.com/ckalgos/fut-trade-enhancer/releases/latest/download/fut-trade-enhancer.user.js",
     ],
     updateURL:
-      "https://github.com/ckalgos/fut-auto-buyer/releases/latest/download/fut-auto-buyer.user.js",
+      "https://github.com/omerkaratas/fut-auto-buyer/releases/latest/download/fut-auto-buyer.user.js",
     downloadURL:
-      "https://github.com/ckalgos/fut-auto-buyer/releases/latest/download/fut-auto-buyer.user.js",
+      "https://github.com/omerkaratas/fut-auto-buyer/releases/latest/download/fut-auto-buyer.user.js",
     noFrame: true,
   },
 };
